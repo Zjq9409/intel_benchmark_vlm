03f91ab400ef78d809212018d752a8e9b8624823
Zhou,jianqian <15205085056@163.com>
2025-05-28 11:44:27 +0800

    add start server

--- nv_benchmark/start_server.sh
+++ nv_benchmark/start_server.sh
@@ -1,25 +1,28 @@
-#export VLLM_USE_V1=1
-#python -m vllm.entrypoints.openai.api_server \
-#    --model /root/jane/Qwen2-VL-7B-Instruct/ \
-#    --tokenizer /root/jane/Qwen2-VL-7B-Instruct/ \
-#    --dtype bfloat16 \
-#    --chat-template /root/jane/vllm/examples/template_chatml.jinja \
-#    --max_num_seqs 256 \
-#    --max-model-len 16384 \
-#    --no-enable-prefix-caching \
-#    --port 8000 \
-#    --host 127.0.0.1
-#
-MODEL="/root/jane/models--RedHatAI--Qwen2-VL-72B-Instruct-FP8-dynamic/"
+#!/bin/bash
+
+MODEL="/root/jane/Qwen2-VL-7B-Instruct/"
+#MODEL="/root/jane/models--RedHatAI--Qwen2-VL-72B-Instruct-FP8-dynamic/"
+
 export VLLM_USE_V1=1
+
+NUM_CARD=1
+# 设置开关变量
+enable_prefix_caching=True  # 改为 False 则禁用 prefix caching
+
+PREFIX_CACHING_FLAG=""
+if [ "$enable_prefix_caching" = "False" ]; then
+    PREFIX_CACHING_FLAG="--no-enable-prefix-caching"
+fi
+
 python -m vllm.entrypoints.openai.api_server \
     --model ${MODEL} \
     --tokenizer ${MODEL} \
     --dtype bfloat16 \
-    --chat-template /root/jane/vllm/examples/template_chatml.jinja \
+    --chat-template ../template_chatml.jinja \
     --max_num_seqs 256 \
     --max-model-len 16384 \
-    --tensor-parallel-size 4 \
     --port 8000 \
-    --host 127.0.0.1
-
+    --tensor-parallel-size ${NUM_CARD} \
+    --trust-remote-code \
+    --host 127.0.0.1 \
+    ${PREFIX_CACHING_FLAG}
