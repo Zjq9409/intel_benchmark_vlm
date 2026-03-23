2669db46fe4d5387d44882ac7cc35410ad05328c
Zhou jianqian <15205085056@163.com>
2026-03-23 15:25:24 +0800

    add profile script

--- performance_benchmark/profile/qwen-vl-4b/torch_start_server.sh
+++ performance_benchmark/profile/qwen-vl-4b/torch_start_server.sh
@@ -6,8 +6,15 @@ export MODEL_PATH="/llm/models/Qwen3-VL-4B-Instruct"
 export MODEL_NAME="Qwen3-VL-4B-Instruct"
 # export LD_LIBRARY_PATH=/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
 
+PROFILE_BASE_DIR="/llm/performance_benchmark/profile/qwen-vl-4b"
+PROFILE_TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+PROFILE_DIR="${PROFILE_BASE_DIR}/${PROFILE_TIMESTAMP}"
+mkdir -p "$PROFILE_DIR"
+
+export VLLM_TORCH_PROFILER_DIR="$PROFILE_DIR"
+
 if [ "$GPU_TYPE" = "XPU" ]; then
-    VLLM_TORCH_PROFILER_DIR=/llm/performance_benchmark/online/profile python3 -m vllm.entrypoints.openai.api_server \
+    python3 -m vllm.entrypoints.openai.api_server \
     --model "$MODEL_PATH" \
     --served-model-name "$MODEL_NAME" \
     --dtype=float16 \
@@ -24,7 +31,7 @@ if [ "$GPU_TYPE" = "XPU" ]; then
     --profiler-config '{"profiler": "torch", "torch_profiler_dir": "./vllm_profile"}' \
     -tp=$TP   
 else
-    VLLM_TORCH_PROFILER_DIR=/llm/performance_benchmark/online/profile python3 -m vllm.entrypoints.openai.api_server \
+    python3 -m vllm.entrypoints.openai.api_server \
     --model "$MODEL_PATH" \
     --served-model-name "$MODEL_NAME" \
     --dtype=float16 \
